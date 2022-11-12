a8df8db17029df42ec55aac54394022561d6816d
sasukenny <kennysuarezh@gmail.com>
2022-11-12 08:48:35 -0500

    release v1.2

--- lib/resources/Registro.dart
+++ lib/resources/Registro.dart
@@ -17,10 +17,11 @@ class Registro {
 
   Future<String> registrar(String nombres, String apellidos, String numero_celular, String correo, String password) async {
     var response = await http.post(
-      Uri.parse('http://192.168.1.2:8081/api/auth/new'),
+      Uri.parse('http://40.112.63.111:8081/api/auth/new'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
+
       body: jsonEncode(<String, String>{
         'name': nombres,
         'apellido': apellidos,
@@ -39,7 +40,7 @@ class Registro {
   Future<String> ingresar(String email, String password) async {
 
     var response = await http.post(
-      Uri.parse('http://192.168.1.2:8081/api/auth/'),
+      Uri.parse('http://40.112.63.111:8081/api/auth/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
